164cc3905c52bf2e0fdc8f368131ea93d0600944
keip777x-oss <keip777x@gmail.com>
2025-11-26 17:34:58 +0900

    main.py を作成

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import asyncio
+from dotenv import load_dotenv
+import discord
+from discord import app_commands
+
+load_dotenv()
+TOKEN = os.getenv("MTQ0MzE1MDQyODY0MzY1OTg5OA.Grg8hS.cdZ9HQX
+_4f5bX9gn2iRsi3LGI7jHoJVKiOyPhE")
+
+if not TOKEN:
+    raise SystemExit("BOT_TOKEN がありません。GitHub Secrets に BOT_TOKEN を追加してください")
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+
+@client.event
+async def on_ready():
+    await tree.sync()
+    print(f"Logged in as {client.user}")
+    print("Slash commands synced!")
+
+
+@tree.command(
+    name="spam",
+    description="指定したメッセージを1〜50回送信します。"
+)
+async def spam(interaction: discord.Interaction, 回数: int, メッセージ: str):
+    if 回数 < 1 or 回数 > 100:
+        await interaction.response.send_message("回数は1〜100で指定してください！", ephemeral=True)
+        return
+
+    await interaction.response.send_message(f"{回数}回送信開始！")
+
+    for i in range(回数):
+        await interaction.channel.send(メッセージ)
+        await asyncio.sleep(0.6)  # レート制限対策
+
+
+client.run(TOKEN)
